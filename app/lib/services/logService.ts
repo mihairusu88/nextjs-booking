cb3532ad2c49b8d1b883cae34ef90317bbd961ad
Ioan-Mihai Rusu <ioan-mihai.rusu@endava.com>
2024-03-21 16:56:38 +0200

    Fix log service

--- app/lib/services/logService.ts
+++ app/lib/services/logService.ts
@@ -2,16 +2,16 @@ const isDevMode = process.env.NODE_ENV === 'development';
 
 const logService = {
   log: (message: any, data?: any) => {
-    !isDevMode || data ? console.log(message, data) : console.log(message);
+    !isDevMode || (data ? console.log(message, data) : console.log(message));
   },
   error: (message: any, data?: any) => {
-    !isDevMode || data ? console.error(message, data) : console.error(message);
+    !isDevMode || (data ? console.error(message, data) : console.error(message));
   },
   warn: (message: any, data?: any) => {
-    !isDevMode || data ? console.warn(message, data) : console.warn(message);
+    !isDevMode || (data ? console.warn(message, data) : console.warn(message));
   },
   info: (message: any, data?: any) => {
-    !isDevMode || data ? console.info(message, data) : console.info(message);
+    !isDevMode || (data ? console.info(message, data) : console.info(message));
   },
 };
 
